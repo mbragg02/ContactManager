a80f480121782fbdad93c6cddbadf7e76cec8957
Michael Bragg <mikeabragg@icloud.com>
2013-12-12 17:46:53 +0000

    Basic start of implementation of contact & contactmanager

---
+++ src/ContactImpl.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+
+public class ContactImpl implements Contact {
+
+	private int id;
+	private String name;
+	private String note;
+	private List<Meeting> meetings;
+	
+	public ContactImpl(int id, String name, String note) {
+		setId(id);
+		setName(name);
+	}
+	
+	private void setId(int id) {
+		this.id = id;
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	@Override
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getNotes() {
+		return this.note;
+	}
+
+	@Override
+	public void addNotes(String note) {
+		this.note = note;
+	}
+
+}
---
+++ src/ContactManagerImpl.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class ContactManagerImpl implements ContactManager {
+	
+	private int contactId;
+	private int meetingId;
+	private List<Contact> contactList;
+	
+	public ContactManagerImpl() {
+		contactId = 0;
+		contactList = new ArrayList<>();
+	}
+
+	@Override
+	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PastMeeting getPastMeeting(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FutureMeeting getFutureMeeting(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Meeting getMeeting(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Meeting> getFutureMeetingList(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Meeting> getFutureMeetingList(Calendar date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PastMeeting> getPastMeetingList(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
+			String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMeetingNotes(int id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewContact(String name, String notes) throws NullPointerException {
+
+		Contact newComtact = new ContactImpl(contactId, name, notes);
+		contactList.add(newComtact);
+		contactId ++ ;
+	}
+
+	@Override
+	public Set<Contact> getContacts(int... ids) {
+		
+		Set<Contact> result = new HashSet<Contact>();
+		
+		for (int id : ids) {
+			Contact aContact = contactList.get(id);
+			// check for null
+			result.add(aContact);
+
+		}
+		return result;
+	}
+
+	@Override
+	public Set<Contact> getContacts(String name) throws NullPointerException {
+		Set<Contact> result = new HashSet<Contact>();
+		int contactsNo = contactList.size();
+		
+		for (int i = 0; i < contactsNo; i ++) {
+			if(contactList.get(i).getName().toLowerCase().contains(name.toLowerCase())) {
+				result.add(contactList.get(i));
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+}
