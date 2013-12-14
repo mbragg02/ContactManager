6d6e753261cf5adc90519d77c9011b48a3fab422
Michael Bragg <mikeabragg@icloud.com>
2013-12-14 09:23:31 +0000

    ContactManagerImpl organized

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -10,17 +10,76 @@ public class ContactManagerImpl implements ContactManager {
 	private int contactId;
 	private int meetingId;
 	private List<Contact> contactList;
+	private List<Meeting> meetingList;
 	
 	public ContactManagerImpl() {
 		contactId = 0;
+		meetingId = 0; 
 		contactList = new ArrayList<>();
+		meetingList = new ArrayList<>();
 	}
+	
+	// Contacts methods
+	
+	@Override
+	public void addNewContact(String name, String notes) throws NullPointerException {
+
+		Contact newComtact = new ContactImpl(contactId, name, notes);
+		contactList.add(newComtact);
+		contactId ++ ;
+	}
+
+	@Override
+	public Set<Contact> getContacts(int... ids) {
+		
+		Set<Contact> result = new HashSet<Contact>();
+		
+		for (int id : ids) {
+			Contact aContact = contactList.get(id);
+			// check for null
+			result.add(aContact);
+
+		}
+		return result;
+	}
+
+	@Override
+	public Set<Contact> getContacts(String name) throws NullPointerException {
+		Set<Contact> result = new HashSet<Contact>();
+		int contactsNo = contactList.size();
+		
+		for (int i = 0; i < contactsNo; i ++) {
+			if(contactList.get(i).getName().toLowerCase().contains(name.toLowerCase())) {
+				result.add(contactList.get(i));
+			}
+		}
+		return result;
+	}
+	
+	
+	
+	// Meetings setters
 
 	@Override
 	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	@Override
+	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
+			String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMeetingNotes(int id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// Meetings getters
 
 	@Override
 	public PastMeeting getPastMeeting(int id) {
@@ -58,54 +117,17 @@ public class ContactManagerImpl implements ContactManager {
 		return null;
 	}
 
-	@Override
-	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
-			String text) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void addMeetingNotes(int id, String text) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void addNewContact(String name, String notes) throws NullPointerException {
-
-		Contact newComtact = new ContactImpl(contactId, name, notes);
-		contactList.add(newComtact);
-		contactId ++ ;
-	}
-
-	@Override
-	public Set<Contact> getContacts(int... ids) {
-		
-		Set<Contact> result = new HashSet<Contact>();
-		
-		for (int id : ids) {
-			Contact aContact = contactList.get(id);
-			// check for null
-			result.add(aContact);
-
-		}
-		return result;
-	}
-
-	@Override
-	public Set<Contact> getContacts(String name) throws NullPointerException {
-		Set<Contact> result = new HashSet<Contact>();
-		int contactsNo = contactList.size();
-		
-		for (int i = 0; i < contactsNo; i ++) {
-			if(contactList.get(i).getName().toLowerCase().contains(name.toLowerCase())) {
-				result.add(contactList.get(i));
-			}
-		}
-		return result;
-	}
-
+	
+	
+	
+	/**
+	 * Save all data to disk. 
+	 * 
+	 * This method must be executed when the program is
+	 * closed and when/if the user requests it. 
+	 */
 	@Override
 	public void flush() {
 		// TODO Auto-generated method stub
